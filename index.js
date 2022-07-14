ac9e8b49c8f14728df71f2b67e35cfb821a9ad69
mascot86 <mascot86@mail.ru>
2022-07-14 10:26:39 +0300

    Add spoiller files

---
+++ index.js
@@ -0,0 +1,17 @@
+// Выбираем все элементы на страницы (на выходе получаем коллекцию элементов, именно коллекцию, а не массив)
+
+const spoiller = document.querySelectorAll(".spoiller-content");
+
+// Первый вариант решения через for, более грамозкий вариант
+
+// let len = spoiller.length;
+
+// for (let i = 0; i < len; i++) {
+//   spoiller[i].addEventListener("click", function () {
+//     this.classList.toggle("active");
+//   });
+// }
+
+// Второй вариант решения в одну строчку с помощью метода перебора foreach, который работает с коллекциями
+
+spoiller.forEach(item => item.addEventListener("click", () => item.classList.toggle("active")));
